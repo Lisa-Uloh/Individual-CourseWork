b1b4c8175516640e42fa4924d28651be9503782d
Lisa Uloh <lissyuloh8@gmail.com>
2023-03-04 22:12:47 +0400

    Index, Images and json

---
+++ js/index.js
@@ -0,0 +1,83 @@
+let courses = [
+    {
+      id: 1,
+      subject: "Arts and Craft",
+      location: "London",
+      price: "$200",
+      noOfSpaces: 5,
+      image:"image/art&craft.jpeg"
+    },
+    {
+        id: 2,
+        subject: "Sports",
+        location: "London",
+        price: "$200",
+        noOfSpaces: 5,
+        image:"image/sports2.jpg"
+    },
+    {
+        id: 3,
+        subject: "Music",
+        location: "London",
+        price: "$200",
+        noOfSpaces: 5,
+        image:"image/music.jpg"
+    },
+    {
+        id: 4,
+        subject: "Theater",
+        location: "London",
+        price: "$250",
+        noOfSpaces: 5,
+        image:"image/theater2.jpeg"
+    },
+    {
+        id: 5,
+        subject: "Coding and Programming",
+        location: "London",
+        price: "$200",
+        noOfSpaces: 5,
+        image:"image/coding.jpg"
+    },
+    {
+        id: 6,
+        subject: "Cooking and Baking",
+        location: "London",
+        price: "$200",
+        noOfSpaces: 5,
+        image:"image/cooking.jpg"
+    },
+    {
+        id: 7,
+        subject: "Science Experiments",
+        location: "London",
+        price: "$300",
+        noOfSpaces: 5,
+        image:"image/scienceexp.jpg"
+    },
+    {
+        id: 8,
+        subject: "Reading and writing",
+        location: "London",
+        price: "$200",
+        noOfSpaces: 5,
+        image:"image/reading.jpg"
+    },
+    {
+        id: 9,
+        subject: "Outdoor Activities",
+        location: "London",
+        price: "$200",
+        noOfSpaces: 5,
+        image:"image/outdoor.jpg"
+    },
+    {
+        id: 10,
+        subject: "Voluteering",
+        location: "London",
+        price: "$300",
+        noOfSpaces: 5,
+        image:"image/volutering.jpg"
+    }
+  ]
+
